7b5eaf3de3741ad179be4c74f0ac046aa708ef77
omar.bonfanti <omar.bonfanti@pgmd.it>
2024-03-13 17:25:06 +0100

    Codice sistemato

--- test1.py
+++ test1.py
@@ -3,6 +3,9 @@ import re
 
 filepath = './MAFFT.fa'
 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 def main():
 
     lista_seq = read_file(filepath)
@@ -11,71 +14,133 @@ def main():
     #rimuoviamo ref dalla lista
     lista_seq.pop(0)
 
-    variazioni(lista_seq, ref)
+    lista_variazioni=search_variation(lista_seq, ref) 
+    stampo_tutte_var(lista_variazioni)
+
+    rep_of_variation(lista_variazioni)
+    print('-----------------------------------------------------------------------------------------------------------------')
+   
+    stamp_max_min(lista_variazioni)
+    print('-----------------------------------------------------------------------------------------------------------------')
+   
+    all_common_position(lista_variazioni)
+    print('-----------------------------------------------------------------------------------------------------------------')
+   
+    all_common_variation(lista_variazioni)
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def read_file(filepath):
-
-    lista_elementi = []
-    nomi_sequenze = []
-    elemento_corrente = ""
+    List_genomi = []
+    temp = ""
 
     with open(filepath, 'r') as file:   
-        for linea in file:
-            if linea.startswith('>'):
-                if elemento_corrente != "":  # Se abbiamo già raccolto una sequenza, la aggiungiamo alla lista
-                    lista_elementi.append(elemento_corrente)
-                    elemento_corrente = ""  
+        for row in file:
+            if row.startswith('>'):
+                if temp != "":  # Se abbiamo già raccolto una sequenza, la aggiungiamo alla lista
+                    List_genomi.append(temp)
+                    temp = ""  
             else:
-                elemento_corrente += linea.strip()  # Aggiungiamo la linea corrente (senza '\n') alla sequenza
+                temp += row.strip()  # Aggiungiamo la linea corrente (senza '\n') alla sequenza
 
-        # Aggiungiamo l'ultimo elemento alla lista, se esiste
-        if elemento_corrente:
-            lista_elementi.append(elemento_corrente)
+        # Aggiungiamo l'ultimo genoma alla lista, se esiste
+        if temp:
+            List_genomi.append(temp)
        
-    return lista_elementi
+    return List_genomi
 
-def variazioni(lista_delle_sequenze, ref):
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def search_variation(List_genomi, ref):
 
     start = 0
     end = len(ref)
-    lista=[]
+    lista_variazioni=[]
 
-    for elemento in lista_delle_sequenze:
-        for i, base in enumerate(elemento): 
+    for genoma in List_genomi:
+        for i, base in enumerate(genoma): 
                 if base == '-': 
                     continue        
                 else:
                     start = i
                     break
                 
-        for i in reversed(range(len(elemento))):
-                if elemento[i] == '-': 
+        for i in reversed(range(len(genoma))):
+                if genoma[i] == '-': 
                     continue        
                 else:
                     end = i+1
                     break       
         
-        listina = []
+        variazioni_genoma = []
 
-        for i, base in enumerate(elemento[start: end]): 
+        for i, base in enumerate(genoma[start: end]): 
             if base != ref[i+start]:
                 if base == '-': 
-                    # print('posizione', i+start, ': inserimento di', ref[i+start])
-                    listina.append(('posizione '+str(i+start+1)+' : inserimento di '+ref[i+start]))
+                   
+                    variazioni_genoma.append(('Posizione '+str(i+start+1)+' : inserimento di '+ref[i+start]))
                 if base == {'A','C','G','T','N'} and ref[i+start] == '-': 
-                    #print('posizione', i+start, ': cancellazione di', base) 
-                    listina.append(('posizione '+str(i+start+1)+' : cancellazione di '+base))
+                   
+                    variazioni_genoma.append(('Posizione '+str(i+start+1)+' : cancellazione di '+base))
                 if base == {'A','C','G','T'} and ref[i+start] =='N' :
                     continue
                 elif base in {'A','C','G','T'} and ref[i+start] != base:
-                    #print('posizione', i+start, ': sostituzione ', ref[i+start], '->', base)
-                    listina.append( ('posizione '+str(i+start+1)+' : sostituzione '+ref[i+start]+' -> '+base))
+                   
+                    variazioni_genoma.append( ('Posizione '+str(i+start+1)+' : sostituzione '+ref[i+start]+' -> '+base))
 
-        lista.append( listina)
+        lista_variazioni.append(variazioni_genoma)
+    return lista_variazioni 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def stampo_tutte_var(lista):
     
-    stampo_tutte_var(lista)
-#   print(lista)
+    lista_nomi=cerco_gen(filepath)
+    for i, nome in enumerate(lista_nomi):
+        print('Variazione di: '+ nome)
+        for i in lista[i]:
+                print(i)
+        print('-----------------------------------------------------------------------------------------------------------------')
 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def cerco_gen(filepath):
+    
+    with open(filepath, 'r') as file:
+        text = file.read()
+        patter = r'>([^ ]+)'
+        corrispondenze = re.findall(patter , text)
+        #rimuoviamo ref dalla lista      
+        corrispondenze.pop(0)
+        
+    return corrispondenze
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def rep_of_variation(lista_variazioni):
+    
+    list_frequency_of_variation = []
+
+    for list_var_genoma in lista_variazioni:
+        for var in list_var_genoma:
+            found = any(tup[0] == var for tup in list_frequency_of_variation)
+            if  found:
+                for index, (string, number) in enumerate(list_frequency_of_variation):
+                    if string == var:
+                        list_frequency_of_variation[index]=(string,(number+1))          
+            else:
+                list_frequency_of_variation.append((var, 1))
+    
+    
+    list_frequency_of_variation = sorted(list_frequency_of_variation, key=lambda x: int(x[0].split()[1]))
+
+    for  (string, number) in list_frequency_of_variation:
+        print(string +' , la variazione ripetto al reference si ripete in '+ str(number) +' genomi')
+
+            
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+def stamp_max_min(lista):
     maxlength=max(len(elemento) for elemento in lista)
     minlength=min(len(elemento) for elemento in lista)
  
@@ -91,76 +156,63 @@ def variazioni(lista_delle_sequenze, ref):
     #for elemento in lista:
         #print(len(elemento))
 
-    print('genoma' ,lista_gen[maxlista] ,'massimo: ', max(len(elemento) for elemento in lista)) 
-    print('genoma' ,lista_gen[minlista] ,'minimo: ', min(len(elemento) for elemento in lista)) 
-    
-    print('-----------------------------------------------------------------------------------------------------------------')
+    print('Il genoma' ,lista_gen[maxlista] ,'è quello con il numero massimo di variazioni: ', max(len(elemento) for elemento in lista)) 
+    print('Il genoma' ,lista_gen[minlista] ,'è quello con il numero minimo di variazioni: ', min(len(elemento) for elemento in lista)) 
 
-    lista_var_in_tutte = []
-     
-    for elemento in lista:
-        if lista_var_in_tutte == []:
-            lista_var_in_tutte = elemento
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+               
+def all_common_position(lista_variazioni):
+    list_all_common_position = []
+    
+    
+    print('Elenco delle posizioni del reference rispetto a cui tutti i genomi variano: ')
+    print()
+    
+    for pos in lista_variazioni:
+        if list_all_common_position == []:
+            list_all_common_position = pos
         else:
-            set1 = {re.match(r'[^:]+',stringa).group() for stringa in lista_var_in_tutte} 
-            set2 = {re.match(r'[^:]+',stringa).group() for stringa in elemento}
-            lista_var_in_tutte = set1.intersection(set2)
+            set1 = {re.match(r'[^:]+',stringa).group() for stringa in list_all_common_position} 
+            set2 = {re.match(r'[^:]+',stringa).group() for stringa in pos}
+            list_all_common_position = set1.intersection(set2)
     
     #ordina le posizioni tramite una lambda expression
-    lista_var_in_tutte = sorted(lista_var_in_tutte, key=lambda x: int(x.split()[1]))
+    list_all_common_position = sorted(list_all_common_position, key=lambda x: int(x.split()[1]))
 
-    for elemento in lista_var_in_tutte:
-        print(elemento)
-
-    print(len(lista_var_in_tutte))
-
-
+    for pos in list_all_common_position:
+        print(pos)
+    print()
+    print('Numero di posizioni del reference rispetto a cui tutti i genomi variano: '+ str(len(list_all_common_position)))
     
-    print('-----------------------------------------------------------------------------------------------------------------')
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+def all_common_variation(lista_variazioni):
 
+    list_all_common_variation = []
+    
+    print('Elenco delle posizioni del reference rispetto a cui tutti i genomi variano allo stesso modo: ')
+    print()
 
-    lista_var_in_tutte_uguale = []
-     
-    for elemento in lista:
-        if lista_var_in_tutte_uguale == []:
-            lista_var_in_tutte_uguale = elemento
+    for var in lista_variazioni:
+        if list_all_common_variation == []:
+            list_all_common_variation = var
         else:
-            set1 = set(lista_var_in_tutte_uguale)
-            set2 = set(elemento)
+            set1 = set(list_all_common_variation)
+            set2 = set(var)
             
-            lista_var_in_tutte_uguale = set1.intersection(set2)
+            list_all_common_variation = set1.intersection(set2)
     
     #ordina le posizioni tramite una lambda expression
-    lista_var_in_tutte_uguale = sorted(lista_var_in_tutte_uguale, key=lambda x: int(x.split()[1]))
+    list_all_common_variation = sorted(list_all_common_variation, key=lambda x: int(x.split()[1]))
 
-    for elemento in lista_var_in_tutte_uguale:
-        print(elemento)
+    for var in list_all_common_variation:
+        print(var)
 
-    print(len(lista_var_in_tutte_uguale))
-
-def cerco_gen(filepath):
-
-    with open(filepath, 'r') as file:
-        testo = file.read()
-        patter = r'>([^ ]+)'
-        corrispondenze = re.findall(patter , testo)
-        #rimuoviamo ref dalla lista      
-        corrispondenze.pop(0)
-        
-    return corrispondenze
+   
+    print()
+    print('Numero di posizioni del reference rispetto a cui tutti i genomi variano allo stesso modo: '+ str(len(list_all_common_variation)))
     
-
-def stampo_tutte_var(lista):
-
-    lista_nomi=cerco_gen(filepath)
-    for i, nome in enumerate(lista_nomi):
-        print(nome)
-        for i in lista[i]:
-                print(i)
-        print('------------------------------------------------------------------------------------')
-
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
                 
 main()
